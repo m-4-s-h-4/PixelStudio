64978d3514b0398c574cd554c9383129a437a67e
Maria <maria123321borovikova@gmail.com>
2024-02-09 13:37:27 +0100

    style adjustemnst

--- src/app/all-projects/Projects.tsx
+++ src/app/all-projects/Projects.tsx
@@ -1,58 +1,43 @@
-/* eslint-disable @next/next/no-img-element */
 "use client";
 
+import { useState } from "react";
 import { useProjects } from "./useProjects";
 import styles from "./Projects.module.css";
 import Link from "next/link";
 
 export default function Projects() {
-  const {
-    filteredProjects,
-    uniqueSummaries,
-    selectedSummary,
-    setSelectedSummary,
-  } = useProjects();
+  const { filteredProjects, uniqueSummaries, setSelectedSummary } = useProjects();
+  const [showFilter, setShowFilter] = useState(false);
+
+  // Toggle dropdown visibility
+  const toggleFilterDropdown = () => setShowFilter(!showFilter);
 
   return (
     <div className={styles.projectList}>
       <div className={styles.summaryContainer}>
-        {/* Filter buttons */}
-        {uniqueSummaries.map((summary) => (
-          <button
-            key={summary}
-            className={`${styles.filterButton} ${
-              selectedSummary === summary ? styles.selectedSummary : ""
-            }`}
-            onClick={() => setSelectedSummary(summary)}
-          >
-            {summary}
-          </button>
-        ))}
-        <button
-          className={styles.filterButton}
-          onClick={() => setSelectedSummary("")}
-        >
-          All
-        </button>
-      </div>
-      {/* Project grid */}
-      <div className={styles.projectGrid}>
         {filteredProjects.map((project) => (
           <div key={project.id} className={styles.project}>
             <Link href={`/projects/${project.id}`}>
-              <div>
-                <div className={styles.projectInner}>
-                  <img
-                    src={project.thumbnail.url}
-                    alt={project.title}
-                    className={styles.projectImage}
-                  />
-                  <div className={styles.projectTitle}>{project.title}</div>
-                </div>
+              <div className={styles.projectInner}>
+                <img src={project.thumbnail.url} alt={project.title} className={styles.projectImage} />
               </div>
             </Link>
           </div>
         ))}
+      </div>
+      <div className={styles.projectsRightSide}>
+        <div className={styles.projectsTitle}>Projects</div> {/* Apply the new class here */}
+        <div className={styles.filterMenu}>
+          <button className={styles.filterButton} onClick={toggleFilterDropdown}>Filter</button>
+          <div className={`${styles.filterDropdown} ${showFilter ? styles.show : ""}`}>
+            {uniqueSummaries.map((summary) => (
+              <a key={summary} onClick={() => {setSelectedSummary(summary); setShowFilter(false);}}>
+                {summary}
+              </a>
+            ))}
+            <a onClick={() => {setSelectedSummary(""); setShowFilter(false);}}>All</a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/theme-swticher.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-export const ThemeSwitcher = () => {
-  const [themeState, setThemeState] = useState(0);
-  useEffect(() => {}, [themeState]);
-  return <pre onClick={() => setThemeState((x) => x + 1)}>{themeState}</pre>;
-};

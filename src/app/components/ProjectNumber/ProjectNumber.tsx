95c2c8221c8f196494e463ae14a07cda9cde24e4
Maria <maria123321borovikova@gmail.com>
2024-02-09 20:10:57 +0100

    project-num-functionalitty

---
+++ src/app/components/ProjectNumber/ProjectNumber.tsx
@@ -0,0 +1,13 @@
+interface ProjectNumberProps {
+  currentIndex: number;
+  className:any;
+}
+
+const ProjectNumber: React.FC<ProjectNumberProps> = ({ currentIndex, className  }) => {
+
+  const formattedIndex = `${currentIndex + 1}`.padStart(2, '0');
+
+  return <span className={className}>{formattedIndex}</span>; 
+};
+
+export default ProjectNumber;
